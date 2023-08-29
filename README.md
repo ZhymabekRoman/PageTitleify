36d9e232b77264a68199ea9e8a837c362c060ab6
ZhymabekRoman <robanokssamit@yandex.ru>
2023-08-29 21:00:50 +0600

    upload: RC release

---
+++ README.md
@@ -0,0 +1,10 @@
+# PageTitleify
+Simple page title parser. Written in Golang.
+
+## TODO:
+ - [ ] Write meta tags parser
+
+## Usage:
+```
+curl --location --request GET 'localhost:8080?parse_type=simple' --header 'Content-Type: text/plain' --data '{url: google.com}'
+```
--- main.go
+++ main.go
@@ -4,25 +4,24 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
-	"golang.org/x/net/html"
+	"errors"
+	// "fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-    "errors"
+
+	"golang.org/x/net/html"
 )
 
 type Page struct {
 	URL string `json:"url"`
 }
 
-func checkURL(URL string, w http.ResponseWriter, r *http.Request) (*url.URL, error) {
-        // Check: https://.com
-        fmt.Printf("BRRRRRRRRRRRRRRRRRRRRR %s", URL)
+func checkURL(URL string) (*url.URL, error) {
 	URLParse, err := url.Parse(URL);
-    if err != nil || URLParse.Scheme == "" || URLParse.Host == "" {
+    if err != nil || URLParse.Host == "" {  // URLParse.Scheme == "" ||
         return URLParse, errors.New("Invalid URL!")
 	}
     return URLParse, nil
@@ -50,69 +49,57 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Printf("Link: %s", page.URL)
-
 	w.Header().Set("Content-Type", "application/json")
 
-	ParseType := r.FormValue(`parse_type`)
-	switch ParseType {
-	case "browser":
-		_, err = checkURL(page.URL, w, r)
-		if err != nil {
-			return
-		}
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(page.URL))
-	case "simple":
-		_, err = checkURL(page.URL, w, r)
-		if err != nil {
-			fmt.Println("ASDDDDDDDDDDDDDDDDDDDDDDDDD")
-    		w.WriteHeader(http.StatusBadRequest)
-	    	json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
-        return
-		}
-
-		resp, err := http.Get(page.URL)
-		if err != nil {
-			//proxy status and err
-			w.WriteHeader(resp.StatusCode)
-			json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
-			return
-		}
-
-		meta := extract(resp.Body)
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(meta)
-
-	default:
-		ErrorMessage := fmt.Sprintf(`Unknown parse_type value: %s`, ParseType)
-		w.WriteHeader(http.StatusBadRequest)
-		response := map[string]string{"error": ErrorMessage}
-		jsonResponse, err := json.Marshal(response)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-		w.Write(jsonResponse)
+    // ParseType := r.FormValue(`parseType`)
+	// switch ParseType {
+	// case "browser":
+	//	_, err = checkURL(page.URL, w, r)
+	//	if err != nil {
+	//		return
+	//	}
+	//	w.WriteHeader(http.StatusOK)
+	//	w.Write([]byte(page.URL))
+	// case "simple":
+	_, err = checkURL(page.URL)
+    if err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
 		return
 	}
 
+	resp, err := http.Get(page.URL)
+	if err != nil {
+			w.WriteHeader(resp.StatusCode)
+			json.NewEncoder(w).Encode(map[string]string{"error": err.Error()})
+			return
+	}
+
+	meta := extract(resp.Body)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(meta)
 }
+
 
 type HTMLMeta struct {
 	Title       string `json:"title"`
+    /*
 	Description string `json:"description"`
 	Image       string `json:"image"`
 	SiteName    string `json:"site_name"`
+    */
 }
 
+// meta property extracter is broken
 func extractMetaProperty(t html.Token, prop string) (content string, ok bool) {
 	for _, attr := range t.Attr {
-		if attr.Key == "property" && attr.Val == prop {
+		if attr.Key == "content" && attr.Val == prop {
+            log.Println("All is OK~! 111111")
 			ok = true
 		}
 
 		if attr.Key == "content" {
+                        log.Println("All is OK~! 222222")
 			content = attr.Val
 		}
 	}
@@ -121,57 +108,57 @@ func extractMetaProperty(t html.Token, prop string) (content string, ok bool) {
 }
 
 func extract(resp io.Reader) *HTMLMeta {
-	z := html.NewTokenizer(resp)
-
-	titleFound := false
-
+	tokenizer := html.NewTokenizer(resp)
 	hm := new(HTMLMeta)
 
 	for {
-		tt := z.Next()
-		switch tt {
+		tokenType := tokenizer.Next()
+        token := tokenizer.Token()
+		switch tokenType {
 		case html.ErrorToken:
 			return hm
 		case html.StartTagToken, html.SelfClosingTagToken:
-			t := z.Token()
-			if t.Data == `body` {
+            //fmt.Println(tokenType)
+            //fmt.Println(token.Data)
+            //fmt.Println(token.String())
+
+			if token.Data == `body` {
 				return hm
 			}
-			if t.Data == "title" {
-				titleFound = true
+            // log.Println(t.Data)
+			if token.Data == "title" {
+                    tokenizer.Next()
+                    token := tokenizer.Token()
+                    hm.Title = token.String()
 			}
-			if t.Data == "meta" {
-				desc, ok := extractMetaProperty(t, "description")
+            /*
+			if token.Data == "meta" {
+				desc, ok := extractMetaProperty(token, "description")
 				if ok {
 					hm.Description = desc
 				}
 
-				ogTitle, ok := extractMetaProperty(t, "og:title")
+				ogTitle, ok := extractMetaProperty(token, "og:title")
 				if ok {
 					hm.Title = ogTitle
 				}
 
-				ogDesc, ok := extractMetaProperty(t, "og:description")
+				ogDesc, ok := extractMetaProperty(token, "og:description")
 				if ok {
 					hm.Description = ogDesc
 				}
 
-				ogImage, ok := extractMetaProperty(t, "og:image")
+				ogImage, ok := extractMetaProperty(token, "og:image")
 				if ok {
 					hm.Image = ogImage
 				}
 
-				ogSiteName, ok := extractMetaProperty(t, "og:site_name")
+				ogSiteName, ok := extractMetaProperty(token, "og:site_name")
 				if ok {
 					hm.SiteName = ogSiteName
 				}
 			}
-		case html.TextToken:
-			if titleFound {
-				t := z.Token()
-				hm.Title = t.Data
-				titleFound = false
-			}
+            */
 		}
 	}
 	return hm
@@ -179,5 +166,6 @@ func extract(resp io.Reader) *HTMLMeta {
 
 func main() {
 	http.HandleFunc("/", home)
+    log.Println("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
